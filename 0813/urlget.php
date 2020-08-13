7a34a5ea00e101f881f5d602c5c16160c29dcbe4
lienero <lienero@naver.com>
2020-08-13 22:19:40 +0900

    0813 ajax 실습

--- 0813/urlget.php
+++ 0813/urlget.php
@@ -1,13 +1,17 @@
 <?php // urlget.php
   if (isset($_GET['url']))
   {
+    //전체 파일을 문자열로 읽습니다.
     echo file_get_contents("http://".sanitizeString($_GET['url']));
   }
 
   function sanitizeString($var)
   {
+    // strip_tags($var) : strip_tags는 문자열에서 HTML 태그와 PHP 태그 제거하는 함수입니다
     $var = strip_tags($var);
+    // htmlentities($var) : 주어진 모든 문자를 html 엔티티로 변환
     $var = htmlentities($var);
+    // stripslashes($var) : 백 슬래시를 제거하는 기능을 가짐
     return stripslashes($var);
   }
 ?>
--- 0813/urlpost.php
+++ 0813/urlpost.php
@@ -1,13 +1,17 @@
 <?php // urlpost.php
   if (isset($_POST['url']))
   {
+    //전체 파일을 문자열로 읽습니다.
     echo file_get_contents("http://" . SanitizeString($_POST['url']));
   }
   
   function SanitizeString($var)
   {
+    // strip_tags($var) : strip_tags는 문자열에서 HTML 태그와 PHP 태그 제거하는 함수입니다
     $var = strip_tags($var);
+    // htmlentities($var) : 주어진 모든 문자를 html 엔티티로 변환
     $var = htmlentities($var);
+    // stripslashes($var) : 백 슬래시를 제거하는 기능을 가짐
     return stripslashes($var);
   }
 ?>
